7afce297bed8973b7921a125a83eb00f566064ad
Erik Onarheim <erik.onarheim@gmail.com>
2024-12-25 17:39:28 -0600

    prep for v0.30.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,81 @@
 const path = require("path");
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
- entry: {
-    'excalibur-spritefusion': './src/index.ts',
-    'excalibur-spritefusion.min': './src/index.ts',
- },
- module: {
-   rules: [
-     {
-       test: /\.ts$/,
-       use: 'ts-loader',
-       exclude: /node_modules/
-     }
-   ]
- },
- mode: 'development',
- devtool: 'source-map',
- devServer: {
-   static: '.',
- },
- resolve: {
-   fallback: {
-      fs: false
-   },
-   extensions: [".ts", ".js"],
-   alias: {
-      "@": path.resolve(__dirname, './src/')
-   }
- },
- output: {
-   filename: "[name].js",
-   path: path.join(__dirname, "dist"),
-   library: ["ex", "Plugin", "SpriteFusion"],
-   libraryTarget: "umd"
- },
- optimization: {
-   minimize: true,
- },
- externals: {
-    "excalibur": {
-       commonjs: "excalibur",
-       commonjs2: "excalibur",
-       amd: "excalibur",
-       root: "ex"
-   }
- },
- plugins: [
-   //  new BundleAnalyzerPlugin()
- ]
+
+/**
+ * @returns {import('webpack').Configuration}
+ */
+module.exports = (env, argv) => {
+  const { mode } = argv;
+
+  const umdOutput = {
+    path: path.resolve(__dirname, 'build/umd'),
+    filename: mode === 'development' ? '[name].development.js' : '[name].js',
+    library: {
+      name: ["ex", "Plugin", "SpriteFusion"],
+      type: 'umd'
+    }
+  };
+
+  const esmOutput = {
+    path: path.resolve(__dirname, 'build/esm'),
+    filename: mode === 'development' ? '[name].development.js' : '[name].js',
+    module: true,
+    library: {
+      type: 'module'
+    }
+  };
+
+  return {
+    resolve: {
+      alias: {
+        "excalibur": path.resolve('./node_modules/excalibur')
+      }
+    },
+    entry: {
+      'excalibur-spritefusion': './src/index.ts',
+      'excalibur-spritefusion.min': './src/index.ts',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              outDir: env.output === 'esm' ? esmOutput.path : umdOutput.path
+            }
+          }
+        },
+      ]
+    },
+    mode,
+    devtool: 'source-map',
+    devServer: {
+      static: '.',
+    },
+    resolve: {
+      fallback: {
+        fs: false
+      },
+      extensions: [".ts", ".js"],
+      alias: {
+        "@": path.resolve(__dirname, './src/')
+      }
+    },
+    output: env.output === 'esm' ? esmOutput : umdOutput,
+    experiments: env.output === 'esm' ? { outputModule: true } : {},
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          include: /\.min\.js$/
+        })
+      ]
+    },
+    externals: ["excalibur"],
+    plugins: [
+      //  new BundleAnalyzerPlugin()
+    ]
+  };
 };
